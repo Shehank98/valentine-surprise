9604df40b33de4524150ffe8a3a8eb6b00a9beb4
Shehan Kavishka <106731265+Shehank98@users.noreply.github.com>
2026-01-31 23:18:55 +0530

    Add cache buster to photoFiles URLs
    Updated photoFiles array to include cache buster for image URLs.

--- script.js
+++ script.js
@@ -26,16 +26,17 @@
 // ============================================
 
 // METHOD A: Google Drive Links (EASIEST & RECOMMENDED)
+// Force image loading with better error handling
 const photoFiles = [
-    'https://drive.google.com/uc?export=view&id=1AawQfRS8-IABWqDVXJcWcyluav18iD5C',
-    'https://drive.google.com/uc?export=view&id=1AawQfRS8-IABWqDVXJcWcyluav18iD5C',
-    'https://drive.google.com/uc?export=view&id=1Zaf-cTXx4yc5EklWNglwMo4pwto_gvc9',
-    'https://drive.google.com/uc?export=view&id=1fA2QfBIXBOWnfY4rXnln4Zg5pVYnWIXc',
-    'https://drive.google.com/uc?export=view&id=1dDACy0WfupW7IOVU_0YmPbvccLGIpTII',
-    'https://drive.google.com/uc?export=view&id=1T2M-MTd9bgn4nTirzaJLHIio_DYNXHh9',
-    'https://drive.google.com/uc?export=view&id=1yTTqjfdr_Lh0HEByQUqL-EU8yb6LADhx',
-    'https://drive.google.com/uc?export=view&id=1HMlJcaxZks2UvZD7480aDvz2AleIR2SL',
-];
+ 'https://drive.google.com/uc?export=view&id=1AawQfRS8-IABWqDVXJcWcyluav18iD5C',
+ 'https://drive.google.com/uc?export=view&id=1AawQfRS8-IABWqDVXJcWcyluav18iD5C',
+ 'https://drive.google.com/uc?export=view&id=1Zaf-cTXx4yc5EklWNglwMo4pwto_gvc9',
+ 'https://drive.google.com/uc?export=view&id=1fA2QfBIXBOWnfY4rXnln4Zg5pVYnWIXc',
+ 'https://drive.google.com/uc?export=view&id=1dDACy0WfupW7IOVU_0YmPbvccLGIpTII',
+ 'https://drive.google.com/uc?export=view&id=1T2M-MTd9bgn4nTirzaJLHIio_DYNXHh9',
+ 'https://drive.google.com/uc?export=view&id=1yTTqjfdr_Lh0HEByQUqL-EU8yb6LADhx',
+ 'https://drive.google.com/uc?export=view&id=1HMlJcaxZks2UvZD7480aDvz2AleIR2SL',
+].map(url => url + '&timestamp=' + new Date().getTime()); // Add cache buster
 
 // METHOD B: Direct Image URLs (Imgur, Cloudinary, etc.)
 // const photoFiles = [
